2c1cdad25507f82368cd064dce256c8948e8d9d1
Isaac Montano <isaac.montano@att.net>
2020-06-19 13:45:46 -0700

    Mid Day

--- client_urb_addWillCallAddress.js
+++ client_urb_addWillCallAddress.js
@@ -4,4 +4,41 @@
  * @NScriptType ClientScript
  */
 
- 
+ define(['N/record'], 
+    function (record) {
+
+        function getWillCallAddress(customerId) {
+            var customerRecord = record.load({
+                type: 'customer',
+                id: customerId
+            });
+
+            customerRecord.findSublistLineWithValue({
+                sublistId: 'addressbook',
+                fieldId: 'addr1',
+                value: '2380 Railroad St.'
+            });
+
+            
+
+        }
+
+    function fieldChanged(context) {
+        var currentRecord = context.currentRecord
+        var fieldId = context.fieldId;
+        var customerId = currentRecord.getValue('entity');
+
+        if (fieldId != 'shipmethod' || currentRecord.getValue(fieldId) != 6) { return; }
+
+        var willCallId = getWillCallAddress(customerId);
+
+        currentRecord.setValue({
+            fieldId: 'shipaddresslist',
+            value: willCallId
+        });
+    }
+
+    return {
+        fieldChanged : fieldChanged
+    }
+ });
